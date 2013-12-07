bbdc40490e1a3f5d38335df7d4941d294fc632f2
zmontesd <zmontesd@gmail.com>
2013-12-06 16:06:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChimeForChange::Application.config.secret_token = '4c7c46e06e5636ef5146add26c9cf4f22d36ef9f8c637ff388e23360aa3844347f030919dc9d5f616ee69c1c78da37c02797e2f309151e6f3499a4851a251c36'
