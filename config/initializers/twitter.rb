84448bafbb2ea0d9eb556e9e76611204fe42bc1a
zmontesd <zmontesd@gmail.com>
2013-12-07 07:51:12 -0800

    update twitter var

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-client = Twitter::REST::Client.new do |config|
+@client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
