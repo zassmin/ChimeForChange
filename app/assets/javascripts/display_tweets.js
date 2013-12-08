d17b1547477018810f263536226b2955cbe6597f
zmontesd <zmontesd@gmail.com>
2013-12-07 17:00:55 -0800

    fixed js bug

--- app/assets/javascripts/display_tweets.js
+++ app/assets/javascripts/display_tweets.js
@@ -5,11 +5,16 @@ $(document).ready(function() {
 var TweetsController = {
   bindEvents: function () {
     $(document).on('ajax:success', 'li', this.onLinkSuccess);
+    $(document).on('ajax:success', 'li.current_tweets', this.onLinkHide);
   },
   onLinkSuccess: function(e, partial, status, xhr) {
     $('li.current_tweets').find('ul').remove();
     $(this).closest('li').append(partial.html);
     $('.current_tweets').removeClass();
     $(this).addClass('current_tweets');
+  },
+  onLinkHide: function(e, partial, status, xhr) {
+    $('li.current_tweets').find('ul').toggle();
+     $('.current_tweets').removeClass();
   }
 }
